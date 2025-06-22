9505f7bfba6fcddb02f1eefa51b3a5982231be83
MD. Shohel Rana <shohel065931@gmail.com>
2025-06-22 12:52:55 +0600

    fgdg

--- js/dark-mode-mysite-script.js
+++ js/dark-mode-mysite-script.js
@@ -7,17 +7,20 @@ jQuery(document).ready(function($){
 });
 
 
+
 document.addEventListener('DOMContentLoaded', function(){
   const toggleBtn = document.getElementById('#dmms-toggle-button');
   if (!toggleBtn) return;
 
+  // Load Saved Mode
   if (localStorage.getItem('dark-mode') === 'enabled') {
     document.body.classList.add('dark-mode');
   }
 
-
+  // Toggle Mode
   toggleBtn.addEventListener('click', function(){
     document.body.classList.toggle('dark-mode');
+
     if (document.body.classList.contains('dark-mode')) {
       localStorage.setItem('dark-mode', 'enabled');
     } else {
